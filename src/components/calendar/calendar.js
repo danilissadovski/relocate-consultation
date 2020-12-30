f5e6c6fb772227e2f27a9a16253490db23359509
danilissadovski <danilissadovski@gmail.com>
2020-12-30 03:46:06 +0200

    add calendar refactoring 2

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { InlineWidget } from "react-calendly";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Calendar = () => {
   return (
     <div>
+      <Link to="/">
+        <Button color="warning">Назад</Button>
+      </Link>
       <InlineWidget url="https://calendly.com/danilissadovski/15min" />
     </div>
   );
